9d0308fec846095a33fb8aee9f030f1eb6744969
TJ <TimothyJCar@gmail.com>
2024-04-05 19:25:10 -0400

    Add function and corresponding button to fill with random colors
    This allows users to draw with multiple colors instead of the standard color

--- script.js
+++ script.js
@@ -14,20 +14,12 @@ function createGrid(numOfSquaresPerSide) {
         gridDivObject[i].style.setProperty(`height`, `calc((600px / ${numOfSquaresPerSide}) - 4px`);
         GRIDCONTAINER.appendChild(gridDivObject[i]);
     }
-    fillGrid();
-}
-
-/* Fill grid parts when hovered over */
-function fillGrid() {
-    let gridParts = document.querySelectorAll(`.grid-container > div`);
-    gridParts.forEach(gridPart => gridPart.addEventListener('mouseenter', function () {
-    gridPart.classList.add(`filled`);
-    }));
 }
 
 /* Insert button at top of page that prompts to choose number of squares per side */
 let promptSquaresPerSide = document.createElement(`button`);
 promptSquaresPerSide.textContent = `Click to choose number of squares per side`;
+document.body.insertBefore(promptSquaresPerSide, GRIDCONTAINER);
 promptSquaresPerSide.addEventListener(`click`, function askSqauresPerSide() {
     let promptSquaresPerSideInput = prompt("How many sqaures per side? Max is 100");
     if (promptSquaresPerSideInput > 100) {
@@ -35,6 +27,39 @@ promptSquaresPerSide.addEventListener(`click`, function askSqauresPerSide() {
         askSqauresPerSide();
     } else {
         createGrid(promptSquaresPerSideInput);
+        fillGridWithStandardColor();
     }
 });
-document.body.insertBefore(promptSquaresPerSide, GRIDCONTAINER);
+
+function fillGridWithStandardColor() {
+    let gridParts = document.querySelectorAll(`.grid-container > div`);
+    gridParts.forEach(gridPart => gridPart.addEventListener('mouseenter', function () {
+    gridPart.style.backgroundColor = `rgb(0, 0, 0)`;
+    gridPart.classList.add(`filled`);
+    }));
+}
+
+/* Insert an additional button to select the random color feature when clicked */
+
+let randomColorsButton = document.createElement(`button`);
+randomColorsButton.textContent = `Draw with random colors`;
+document.body.insertBefore(randomColorsButton, GRIDCONTAINER);
+randomColorsButton.addEventListener(`click`, fillGridWithRandomColor);
+
+function fillGridWithRandomColor() {
+    let gridParts = document.querySelectorAll(`.grid-container > div`);
+    gridParts.forEach(gridPart => gridPart.addEventListener('mouseenter', function () {
+    gridPart.style.backgroundColor = rgbValue();
+    gridPart.classList.add(`filled`);
+    }));
+}
+
+/* Get Random RGB value */
+
+function rgbValue() {
+    let first = Math.floor(Math.random() * 256);
+    let second = Math.floor(Math.random() * 256);
+    let third = Math.floor(Math.random() * 256);
+    let rgbValue = `rgb(${first}, ${second}, ${third})`;
+    return rgbValue;
+}
